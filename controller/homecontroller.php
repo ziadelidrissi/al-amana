486fc33bca5353dadd4381f09b8bb831b2b4fab7
MoulayZiad <ziadelidrissi1@gmail.com>
2023-07-19 11:57:03 +0200

    Feat: router + controller first draft

---
+++ controller/homecontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+function accueil()
+{
+    require('view/accueil');
+}
+function galerie()
+{
+    require('view/galerie');
+}
+function evenements()
+{
+    require('view/evenements');
+}
+function en_savoir_plus()
+{
+    require('view/en_savoir_plus');
+}
+function adherez()
+{
+    require('view/adherez');
+}
+function mentions_legales()
+{
+    require('view/mentions_legales');
+}
+
+?>
