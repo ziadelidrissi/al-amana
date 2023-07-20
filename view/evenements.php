4535f938b2191bd45128df1ac94e1ceccb5f1d6c
MoulayZiad <ziadelidrissi1@gmail.com>
2023-07-20 14:01:47 +0200

    Feat: evenements frontend first draft

---
+++ view/evenements.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="dist/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- HERO -->
+    <div class="hero min-h-screen" style="background-image: url(assets/img/hero-evenements.png);"  >
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Retrouvez ici tous nos événements</h1>
+            </div>
+        </div>
+    </div>
+    <!-- HERO -->
+
+    <!-- EVENEMENTS -->
+    <section class="flex flex-col items-center my-20 mx-5 sm:mx-10 md:mx-20 lg:mx-32">
+
+        <!-- CARDS -->
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://picsum.photos/id/684/600/400" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Evenement 1</h2>
+                    <p>Voir evenement 1</p>
+                </div>
+            </div>
+
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://picsum.photos/id/685/600/400" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Evenement 2</h2>
+                    <p>Voir evenement 2</p>
+                </div>
+            </div>
+
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://picsum.photos/id/686/600/400" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Evenement 3</h2>
+                    <p>Voir evenement 3</p>
+                </div>
+            </div>
+
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://picsum.photos/id/687/600/400" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Evenement 4</h2>
+                    <p>Voir evenement 3</p>
+                </div>
+            </div>
+
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src="https://picsum.photos/id/688/600/400" alt=""></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Evenement 5</h2>
+                    <p>Voir evenement 4</p>
+                </div>
+            </div>
+            
+        </div>
+        <!-- CARDS -->
+
+    </section>
+    <!-- EVENEMENTS -->
+
+</body>
+</html>
